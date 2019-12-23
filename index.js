6599353080e063baa398473f11c376b35e253f09
amansethi981 <amansethi981@gmail.com>
2019-12-24 00:23:58 +0530

    updated index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+   
+        var v = document.getElementById("Test_Video");
+
+        function My_VideoRate() {
+            alert(v.playbackRate);
+        }
+
+        function Set_VideoRate() {
+            v.playbackRate = 2;
+        } 
+  
